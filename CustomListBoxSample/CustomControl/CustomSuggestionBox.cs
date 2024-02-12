720480988ecd74498aa59bd6ecb91eec227bfe2c
Anandhan <anandhan.rajagopal@syncfusion.com>
2024-02-12 18:26:29 +0530

    Added Sample

---
+++ CustomListBoxSample/CustomControl/CustomSuggestionBox.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml;
+
+namespace CustomListBoxSample
+{
+    public class CustomSuggestionBox : ListBox, IDisposable
+    {
+
+        private ListBoxItem listboxitem;
+      
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomSuggestionBox" /> class.
+        /// </summary>
+        public CustomSuggestionBox()
+        {
+            this.SelectionChanged += this.SuggestionBox_SelectionChanged;
+            this.Tapped += SuggestionBox_Tapped;
+        }
+
+        /// <summary>
+        /// Prepare container for item override
+        /// </summary>
+        /// <param name="element">Dependency object</param>
+        /// <param name="item">object type</param>
+        protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
+        {
+            System.Diagnostics.Debug.WriteLine("CustomSuggesionBox-------PrepareContainerForItemOverride");
+            this.listboxitem = element as ListBoxItem;
+
+            listboxitem.Loaded -= Listboxitem_Loaded;
+            listboxitem.Loaded += Listboxitem_Loaded;
+            listboxitem.Unloaded -= Listboxitem_Unloaded;
+            listboxitem.Unloaded += Listboxitem_Unloaded;
+         
+            base.PrepareContainerForItemOverride(element, item);
+        }
+
+        private void Listboxitem_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+        }
+     
+        private void Listboxitem_Unloaded(object sender, RoutedEventArgs e)
+        {
+            ListBoxItem listBoxItem = sender as ListBoxItem;
+            if (listBoxItem != null)
+            {
+                System.Diagnostics.Debug.WriteLine("CustomSuggesionBox-------Unsubscribe the events");
+                listBoxItem.Loaded -= Listboxitem_Loaded;
+                listBoxItem.Unloaded -= Listboxitem_Unloaded;
+            }
+        }
+     
+        private void SuggestionBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void SuggestionBox_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            GC.SuppressFinalize(this);
+            if (this.listboxitem != null)
+            {
+                this.listboxitem = null;
+            }
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            this.Tapped -= SuggestionBox_Tapped;
+            if (this.listboxitem != null)
+            {
+               // this.listboxitem.DataContextChanged -= this.Listboxitem_DataContextChanged;
+                this.listboxitem = null;
+            }
+            this.SelectionChanged -= this.SuggestionBox_SelectionChanged;
+            this.ItemsSource = null;
+
+            this.Items.Clear();
+        }
+    }
+
+}
---
+++ CustomListBoxSample/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CustomListBoxSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+      
+            public MainPage()
+            {
+                Employees = new List<Employee>();
+                this.InitializeComponent();
+
+            }
+
+        private List<Employee> employees;
+
+        public List<Employee> Employees
+
+        {
+
+            get { return employees; }
+
+            set { employees = value; }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            popup.IsOpen = !popup.IsOpen;
+        }
+
+        private void popup_Opened(object sender, object e)
+        {
+            List<object> max1 = new List<object>();
+
+
+            max1.Add(new Employee { Name = "Lucas", Email = "lucas@syncfusion.com" });
+
+            max1.Add(new Employee { Name = "James", Email = "james@syncfusion.com" });
+
+            max1.Add(new Employee { Name = "Jacob", Email = "jacob@syncfusion.com" });
+
+            CustomBox.ItemsSource = max1;
+        }
+    }
+
+    public class Employee
+
+    {
+
+        public string Name { get; set; }
+
+
+
+        public string Email { get; set; }
+
+    }
+}
